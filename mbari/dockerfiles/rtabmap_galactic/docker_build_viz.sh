c8c012190e2b2b1317cdf115c8cba301dcacc37c
Simo Linkola <simo.linkola@gimrobotics.fi>
2024-03-20 10:53:24 +0200

    Changed both humble and galactic Dockerfile to use mbari-user from root and default folder inside the container to be /home/mbari/ instead of /root/.

--- mbari/dockerfiles/rtabmap_galactic/docker_build_viz.sh
+++ mbari/dockerfiles/rtabmap_galactic/docker_build_viz.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t mbari/rtabmap_ros_viz:latest -f Dockerfile.viz .
+docker build -t mbari/galactic/rtabmap_ros_viz:latest -f Dockerfile.viz .
--- mbari/dockerfiles/rtabmap_humble/docker_build.sh
+++ mbari/dockerfiles/rtabmap_humble/docker_build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -t mbari/rtabmap_ros:latest --build-arg NUM_THREADS=16 --build-arg READ_TOKEN=$@ .
+docker build -t mbari/humble/rtabmap_ros:latest \
+    --build-arg NUM_THREADS=8 \
+    --build-arg READ_TOKEN=$@ \
+	--build-arg USER_ID=$(id -u) \
+	--build-arg GROUP_ID=$(id -g) .
