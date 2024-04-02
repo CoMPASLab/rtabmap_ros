e11bd365161d51f247dc966bd0cd48489b8f53d1
Tomi Laurinen <tomi.laurinen@gimrobotics.fi>
2024-04-02 12:22:07 +0300

    Remove obsolete Docker-related files

--- mbari/dockerfiles/rtabmap_galactic/docker_build_viz.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t mbari/galactic/rtabmap_ros_viz:latest -f Dockerfile.viz .
--- mbari/dockerfiles/rtabmap_humble/docker_build_viz.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t mbari/humble/rtabmap_ros_viz:latest -f Dockerfile.viz .
